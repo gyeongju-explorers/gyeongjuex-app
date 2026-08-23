26bae77c53f947619403d04a3e40ee024ff1f1ad
sky121016 <sky121016@naver.com>
2026-08-23 17:25:13 +0900

    color 추가

--- src/components/Home/HomeButton.tsx
+++ src/components/Home/HomeButton.tsx
@@ -4,8 +4,8 @@ import { Text } from '@/components/global/text';
 
 const HomeButton = () => {
   return (
-    <Pressable className="h-50 w-140 items-center justify-center rounded-full bg-black">
-      <Text className="text-center text-sm font-bold text-red-500">버튼</Text>
+    <Pressable className="h-50 w-140 items-center justify-center rounded-full bg-background-element dark:bg-background-element-dark">
+      <Text className="text-center text-sm font-bold text-gray-500">버튼</Text>
     </Pressable>
   );
 };
