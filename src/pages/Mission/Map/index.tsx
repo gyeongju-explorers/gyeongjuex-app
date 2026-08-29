b71acadb0b6789c76dee8082a1c4b52b1ec757a7
sky121016 <sky121016@naver.com>
2026-08-29 15:05:09 +0900

    [feat/#16] PhotoMarker 구현

--- src/pages/Mission/Map/index.tsx
+++ src/pages/Mission/Map/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import image1 from '@/assets/images/image1.png';
+import image2 from '@/assets/images/image2.png';
 import { ThemedText } from '@/components/global/themed-text';
 import { ThemedView } from '@/components/global/themed-view';
 import MissionChipList from '@/components/Mission/MissionChipList';
-import { MaxContentWidth } from '@/constants/theme';
+import MissionSlide from '@/components/Mission/MissionSlide';
+import PhotoMarker from '@/components/Mission/PhotoMarker';
+import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 export default function MissionMap() {
   return (
@@ -14,6 +18,9 @@ export default function MissionMap() {
         <View>
           <MissionChipList />
         </View>
+        <PhotoMarker photos={[image1]} />
+        <PhotoMarker photos={[image1, image1]} />
+        <MissionSlide photos={[image1, image2, image1, image2]} />
       </SafeAreaView>
     </ThemedView>
   );
@@ -24,9 +31,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     flexDirection: 'row',
+    backgroundColor: '#333333',
   },
   safeArea: {
     flex: 1,
+    paddingHorizontal: Spacing.four,
+    paddingBottom: BottomTabInset + Spacing.three,
     maxWidth: MaxContentWidth,
   },
 });
---
+++ src/types/assets.d.ts
@@ -0,0 +1,9 @@
+declare module '*.svg' {
+  const value: number;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: number;
+  export default value;
+}
